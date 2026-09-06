cf2c28f7f9c1966efa2137bb684c3c881598bfa0
Apurva Jaiswal <apurvajaiswal7788@gmail.com>
2026-09-06 23:16:18 +0530

    Print a Student ID Card

---
+++ Practice2.java
@@ -0,0 +1,11 @@
+public class Practice2 {
+    public static void main(String[] args) {
+        // Print the student profile information.
+        System.out.println("--------------------");
+        System.out.println("STUDENT PROFILE");
+        System.out.println("Name: Alex");
+        System.out.println("Course: Java");
+        System.out.println("Level: Beginner");
+        System.out.println("--------------------");
+    }
+}
