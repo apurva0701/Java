23494a5bb843a615eee9801e60674bbcf8b1158f
Apurva Jaiswal <apurvajaiswal7788@gmail.com>
2026-09-18 15:36:11 +0530

    Complete even odd check

---
+++ Practice15.java
@@ -0,0 +1,11 @@
+public class Practice15 {
+    public static void main(String[] args) {
+        int number = 14;
+
+        if (number % 2 == 0) {
+            System.out.println("Even");
+        } else {
+            System.out.println("Odd");
+        }
+    }
+}
