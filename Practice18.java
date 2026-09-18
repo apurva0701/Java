ed722e25de3cd6fcc1370afd17e25603b0d6359c
Apurva Jaiswal <apurvajaiswal7788@gmail.com>
2026-09-18 15:39:59 +0530

    Complete print numbers program

---
+++ Practice18.java
@@ -0,0 +1,9 @@
+public class Practice18 {
+    public static void main(String[] args) {
+        int limit = 5;
+
+        for (int i = 1; i <= limit; i++) {
+            System.out.println(i);
+        }
+    }
+}
