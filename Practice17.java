1f83a679c1995a967a0d4e2d2b35a5e63789d3f3
Apurva Jaiswal <apurvajaiswal7788@gmail.com>
2026-09-18 15:38:37 +0530

    Complete student grade program

---
+++ Practice17.java
@@ -0,0 +1,15 @@
+public class Practice17 {
+    public static void main(String[] args) {
+        int marks = 76;
+
+        if (marks >= 80) {
+            System.out.println("Grade: A");
+        } else if (marks >= 60) {
+            System.out.println("Grade: B");
+        } else if (marks >= 40) {
+            System.out.println("Grade: C");
+        } else {
+            System.out.println("Grade: Fail");
+        }
+    }
+}
