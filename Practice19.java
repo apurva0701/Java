0a69fc849e9dcf93d98d34db063368ed8baf0ec0
Apurva Jaiswal <apurvajaiswal7788@gmail.com>
2026-09-18 15:41:09 +0530

    Print  multiplication table

---
+++ Practice19.java
@@ -0,0 +1,9 @@
+public class Practice19 {
+    public static void main(String[] args) {
+        int number = 5;
+
+        for (int i = 1; i <= 5; i++) {
+            System.out.println(number + " x " + i + " = " + (number * i));
+        }
+    }
+}
