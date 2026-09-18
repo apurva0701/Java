b59425b7f04e92bbaee7c2aa8a774f8d5646bde8
Apurva Jaiswal <apurvajaiswal7788@gmail.com>
2026-09-18 15:37:40 +0530

    Complete larger number program

---
+++ Practice16.java
@@ -0,0 +1,14 @@
+public class Practice16 {
+    public static void main(String[] args) {
+        int first = 25;
+        int second = 40;
+
+        if (first > second) {
+            System.out.println("Larger: " + first);
+        } else if (second > first) {
+            System.out.println("Larger: " + second);
+        } else {
+            System.out.println("Equal");
+        }
+    }
+}
