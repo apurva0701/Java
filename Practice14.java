a93c6f780144d36f9e4162ed4031fcb6f2eb5728
Apurva Jaiswal <apurvajaiswal7788@gmail.com>
2026-09-18 15:35:06 +0530

    Complete positive negative check

---
+++ Practice14.java
@@ -0,0 +1,13 @@
+public class Practice14 {
+    public static void main(String[] args) {
+        int number = -8;
+
+        if (number > 0) {
+            System.out.println("Positive");
+        } else if (number < 0) {
+            System.out.println("Negative");
+        } else {
+            System.out.println("Zero");
+        }
+    }
+}
