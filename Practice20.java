6bc517dae1141c1a3fc7b40b8389395475c7755d
Apurva Jaiswal <apurvajaiswal7788@gmail.com>
2026-09-25 15:36:22 +0530

    Add interactive learner profile

---
+++ Practice20.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Practice20 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String firstName = scanner.next();
+        int solvedProblems = scanner.nextInt();
+        double assessmentPercentage = scanner.nextDouble();
+
+        System.out.println("Learner: " + firstName);
+        System.out.println("Problems solved: " + solvedProblems);
+        System.out.println("Assessment: " + assessmentPercentage);
+
+        scanner.close();
+    }
+}
